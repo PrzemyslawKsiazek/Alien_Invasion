0d2e25541875b40604e2960fb2fd535fddf11b7d
Vickul <pksiazek@protonmail.com>
2021-03-03 11:46:45 +0100

    adding a new file where the game setting are stored

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class designed to store all game settings."""
+
+    def __init__(self):
+        """Initialize game settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
