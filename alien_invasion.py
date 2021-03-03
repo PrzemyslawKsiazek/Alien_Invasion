b31f9c33dde885b3538be8f4a045d63e9ab1c2db
Vickul <pksiazek@protonmail.com>
2021-03-03 11:50:31 +0100

    update in the main file under the cew class with settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,17 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """A general class designed to magage resources and hot the gaame works"""
 
     def __init__(self):
         """Initializing the game and creating its resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
-        #Define the background color
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Starting the main game loop."""
@@ -23,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             #Refresh the screen ruring each loop iteration
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             #Display the last modified screen
             pygame.display.flip()
