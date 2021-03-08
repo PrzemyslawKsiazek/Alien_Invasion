bc7f585c9d9550c3ee94eebd55200c3ffed1091c
Vickul <pksiazek@protonmail.com>
2021-03-08 19:04:56 +0100

    adding option to close the application using the Q key

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,6 +41,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Response to key release"""
