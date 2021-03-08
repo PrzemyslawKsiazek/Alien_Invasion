b6f98c2bd1315f8645d33e149c364bc18a7d6c67
Vickul <pksiazek@protonmail.com>
2021-03-08 18:38:57 +0100

    limiting the range of movement of the ship

--- ship.py
+++ ship.py
@@ -33,9 +33,9 @@ class Ship:
             self.rect.x -= 1
 
         #Update the X coordinate value of a ship, not its rectangle
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # update the rect object based on the value of self.x
