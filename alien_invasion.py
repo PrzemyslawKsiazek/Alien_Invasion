2313a1a97b0cb93fa224ba01e73150afa6fff9ae
Vickul <pksiazek@protonmail.com>
2021-03-24 18:59:15 +0100

    improved operation of the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -58,6 +58,25 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Starting a new game when the user clicks the PLAY button"""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            #Resetting game statistics
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            #Deleting the contents of alien and bullets lists
+            self.aliens.empty()
+            self.bullets.empty()
+
+            #Displaing the new fleet and centering the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         """Response to key press"""
