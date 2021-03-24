8ab82b218e3476174ff1831aab01ae2526260512
Vickul <pksiazek@protonmail.com>
2021-03-24 18:44:28 +0100

    revised button.py

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 import pygame.font
 
 class Button():
-    def __init__(self, ai_game, screen, msg):
+    def __init__(self, ai_game, msg):
         """Initialize button attributes"""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
